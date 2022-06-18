7728e873985d638af11cac6dde03cbb5cc6918e0
Unicorn Barf <nolan.spence7@gmail.com>
2022-06-18 01:52:57 -0700

    Finished Nodejs algorithims

--- NodeJS/03-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ NodeJS/03-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,13 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function(arr) {
+    let prodArr = [arr[0]];
+    arr.forEach(element => {
+        if (element > prodArr[0]) {
+            prodArr.unshift(element);
+        }
+    });
+    return prodArr[0] * prodArr[1];
+};
+
+console.log(productOfLargestTwo([-10,-5,-2,-15,-1,-33,-88,-100]));
--- NodeJS/03-Algorithms/03-camel-case/Unsolved/camel-case.js
+++ NodeJS/03-Algorithms/03-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,17 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function(str) {
+    str = str.toLowerCase();
+    let charArr = str.split('');
+    charArr.forEach((element, index) => {
+        if (element === ' ') {
+            charArr.splice(index+1, 1, charArr[index+1].toUpperCase());
+            console.log('hit');
+        }
+    })
+    let newStr = charArr.filter(element => element !== ' ');
+    return newStr.join('');
+};
+
+console.log(camelCase('Hello World'));
+console.log(('Hello World').split(''));
