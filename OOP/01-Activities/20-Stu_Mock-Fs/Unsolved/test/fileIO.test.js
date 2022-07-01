f6a44290059f27f26e2f9cc7218253ffbe8a2d94
Unicorn Barf <nolan.spence7@gmail.com>
2022-07-01 15:12:02 -0700

    Worked on understanding mocks

--- OOP/01-Activities/20-Stu_Mock-Fs/Unsolved/test/fileIO.test.js
+++ OOP/01-Activities/20-Stu_Mock-Fs/Unsolved/test/fileIO.test.js
@@ -1,16 +1,28 @@
 const FileIO = require('../fileIO');
+const fs = require('fs');
 
 describe('FileIO', () => {
   describe('read', () => {
     it("should call fs.readFileSync with the passed in 'file' argument", () => {
       // TODO: Your code here
-      
+      const myFiler = new FileIO();
+      const mock = jest.spyOn(fs, 'readFileSync');
+      mock.mockImplementation(() => {});
+      myFiler.read('read');
+      expect(mock).toBeCalledWith('read', 'utf8');
+      mock.mockRestore();
     });
   });
 
   describe('write', () => {
     it("should call fs.writeFileSync with the passed in 'path' and 'data' arguments", () => {
       // TODO: Your code here
+      const myFiler = new FileIO();
+      const mock = jest.spyOn(fs, 'writeFileSync');
+      mock.mockImplementation(() => {});
+      myFiler.write('read', 'data');
+      expect(mock).toBeCalledWith('read', 'data');
+      mock.mockRestore();
     });
   });
 });
