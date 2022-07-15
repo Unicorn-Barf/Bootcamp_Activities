22902c01066a37c050e9c0494432465d4ad6084d
Unicorn Barf <nolan.spence7@gmail.com>
2022-07-14 17:22:26 -0700

    Fixed schema and seeds to correct structure from sample image.

--- SQL/01-Activities/28-Stu_Mini-Project/db/seeds.sql
+++ SQL/01-Activities/28-Stu_Mini-Project/db/seeds.sql
@@ -5,9 +5,9 @@ VALUES ('Spirited Away'),
     ('Lady Snowblood'),
     ('The Onion Movie');
 
-INSERT INTO reviews (movieId, user, review)
-VALUES (1, 'Bad guy', 'My mind was blown!'),
-    (2, 'Christina', 'Anna Ferris is my girl!'),
-    (3, 'Divorcee', 'I want a free puppy too!'),
-    (4, 'Samurai', 'Much sword, so blood!'),
-    (5, 'Chef Betch', 'Well it definitely wasnt a cooking movie.');
+INSERT INTO reviews (movieId, review)
+VALUES (1, 'My mind was blown!'),
+    (2, 'Anna Ferris is my girl!'),
+    (3, 'I want a free puppy too!'),
+    (4, 'Much sword, so blood!'),
+    (5, 'Well it definitely wasnt a cooking movie.');
